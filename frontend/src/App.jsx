21bd8f8c63eafa65ccc029157ed8053901b5f733
prajjwal-17 <prajjwalchamp17@gmail.com>
2025-02-10 00:53:12 +0530

    Basic  Navbar Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,75 @@
-import {BrowserRouter, Routes, Route, Link, useNavigate, redirect} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Ruler } from "lucide-react";
+import "./App.css";
+import "./Navbar.css";
 
-
-function App(){
-  return <div>
+function App() {
+  return (
     <BrowserRouter>
-    <a href="/">ALLEN(THis will refetch)</a>  ||<Link to={"/login"}>Login</Link> | <Link to={"/signup"}>Signup</Link>
-     <Routes>
-       <Route path="/login" element={<LoginPage/>} />
-       <Route path="/signup" element={<SignupPage/>} />
-       <Route path='/' element={<LandingPage/>}/>
-     </Routes>
-     Common Footer || Bottom 
+      <nav className="navbar">
+        {/* Logo and Company Name */}
+        <div className="logo-container">
+          <Link to="/" className="logo-link">
+            <Ruler className="logo" />
+            <h1 className="site-title">Random Company</h1>
+          </Link>
+        </div>
+
+        {/* Navigation Links */}
+        <div className="nav-links">
+          <Link to="/product">Product</Link>
+          <Link to="/customers">Customers</Link>
+          <Link to="/company">Company</Link>
+          <Link to="/pricing">Pricing</Link>
+          <Link to="/changelog">Changelog</Link>
+        </div>
+
+        {/* Buttons (Sign In & Get Started) */}
+        <div className="auth-buttons">
+          <Link to="/signin" className="btn signin-btn">Sign In</Link>
+          <Link to="/get-started" className="btn get-started-btn">Get Started</Link>
+        </div>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product" element={<ProductPage />} />
+        <Route path="/customers" element={<CustomersPage />} />
+        <Route path="/company" element={<CompanyPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route path="/changelog" element={<ChangelogPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/get-started" element={<GetStartedPage />} />
+      </Routes>
     </BrowserRouter>
-    
-  </div>
+  );
 }
-function LoginPage(){
-    return <div>
-      Login PAge
-    </div>
+
+// Dummy Pages
+function HomePage() {
+  return <div>Welcome to Random Company</div>;
 }
-function SignupPage(){
-    return <div>
-      Signup PAge
-    </div>
+function ProductPage() {
+  return <div>Product Information</div>;
 }
-function LandingPage(){
-  return <div>
-    Welcome
-  </div>
+function CustomersPage() {
+  return <div>Customers</div>;
 }
+function CompanyPage() {
+  return <div>Company Info</div>;
+}
+function PricingPage() {
+  return <div>Pricing Details</div>;
+}
+function ChangelogPage() {
+  return <div>Changelog</div>;
+}
+function SignInPage() {
+  return <div>Sign In Page</div>;
+}
+function GetStartedPage() {
+  return <div>Get Started Page</div>;
+}
+
 export default App;
